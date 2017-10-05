8e64fa0331ad9cf86bd14713c3061672540e7284
Remmy Clay <remmy.lauren@gmail.com>
2017-10-05 05:42:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var name = "Susan"
-var height = "74"
+var height = 74
 if (name === "Susan") {
   console.log("The name is correct")
 }
 else {
   console.log ("Expected the name to be Susan")
 }
-if (height === "74") {
+if (height === 74) {
   console.log("The height is correct")
 }
 else {
